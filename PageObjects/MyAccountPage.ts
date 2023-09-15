def3d37b0c08cc3acac40f799649d2dd20c6899e
uderk <venislav.zdravkov@gmail.com>
2023-09-15 17:20:50 +0300

    MyAccount Page refactored to extend BasePage

--- PageObjects/MyAccountPage.ts
+++ PageObjects/MyAccountPage.ts
@@ -1,7 +1,9 @@
 import { expect, Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
 
-export class MyAccountPage {
+export class MyAccountPage extends BasePage {
   constructor(readonly page: Page, readonly username: string) {
+    super(page);
     this.page = page;
     this.username = username;
   }
