4227e545394291a0c4bac5d92a0f2ce617d676ec
uderk <venislav.zdravkov@gmail.com>
2023-09-15 14:14:30 +0300

    added beforeEach hook

--- e2e/LoginTests.spec.ts
+++ e2e/LoginTests.spec.ts
@@ -2,20 +2,15 @@ import { test, expect, type Page } from "@playwright/test";
 import { LoginPage } from "../PageObjects/LoginPage";
 
 test.describe("Testing the login page", async () => {
-  // test.beforeEach(async ({ page }) => {
-  // const loginPage = new LoginPage(
-  //   page,
-  //   "testUser1@abv.bg",
-  //   "TestPassword!@1"
-  // );
+  let loginPage: LoginPage;
+  let page: Page;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page, "testUser1@abv.bg", "TestPassword!@1");
+    await loginPage.goToLoginPage();
+  });
 
   test("Validate Login Page", async ({ page }) => {
-    const loginPage = new LoginPage(
-      page,
-      "testUser1@abv.bg",
-      "TestPassword!@1"
-    );
-    await loginPage.goToLoginPage();
     await expect(loginPage.titleLocator).toBeAttached();
     await expect(page).toHaveTitle("My account – mystore");
     await expect(loginPage.loginHeaderLocator).toBeVisible();
@@ -27,8 +22,5 @@ test.describe("Testing the login page", async () => {
     await expect(loginPage.passwordLabelLocator).toContainText("Password ");
     await expect(loginPage.rememberMeCheckBoxLocator).toBeEnabled();
     await expect(loginPage.loginButtonLocator).toBeVisible();
-    // await loginPage.enterLoginName(loginPage.username);
-    // await loginPage.enterPassword(loginPage.password);
-    // await loginPage.clickLoginButton();
   });
 });
